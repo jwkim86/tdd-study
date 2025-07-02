ca2f9b73122a34f2636c94a6d9a960a3646a7d3d
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-03 00:55:41 +0900

    first commit

---
+++ src/test/java/com/koreaap/itdev2/TddstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koreaap.itdev2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
