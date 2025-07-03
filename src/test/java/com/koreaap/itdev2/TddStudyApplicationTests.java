73da6951c6fb3e91fcda5952b3adaae3b7a6e91b
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-03 23:00:36 +0900

    Fix typo

--- src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
+++ src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TddstudyApplicationTests {
+class TddStudyApplicationTests {
 
   @Test
   void contextLoads() {}
