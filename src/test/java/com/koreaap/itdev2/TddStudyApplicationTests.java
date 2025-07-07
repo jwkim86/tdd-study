afc9ae231c4ccc5ad1712d84ae66b3a8248fc1f9
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-08 01:18:24 +0900

    Add multiplication test

--- src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
+++ src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
@@ -3,9 +3,18 @@ package com.koreaap.itdev2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class TddStudyApplicationTests {
 
   @Test
   void contextLoads() {}
+
+  @Test
+  public void testMultiplication() {
+    Dollar five = new Dollar(5);
+    five.times(2);
+    assertEquals(new Dollar(10), five.amount);
+  }
 }
