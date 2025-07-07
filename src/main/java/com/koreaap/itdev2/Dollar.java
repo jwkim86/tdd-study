850a4112ae34f77dfc04c1daaee191570c84b99a
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-08 01:36:46 +0900

    Change amount value from constructor parameter

--- src/main/java/com/koreaap/itdev2/Dollar.java
+++ src/main/java/com/koreaap/itdev2/Dollar.java
@@ -2,10 +2,12 @@ package com.koreaap.itdev2;
 
 public class Dollar {
 
-  Dollar(int amount) {}
+  Dollar(int amount) {
+    this.amount = amount;
+  }
 
   void times(int multiplier) {
-    amount = 5 * 2;
+    amount = amount * 2;
   }
 
   int amount;
