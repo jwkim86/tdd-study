3ce34647874094cdd4ca1b8132fc83c902fb3389
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-09 00:30:20 +0900

    Remove Franc.equals

--- src/main/java/com/koreaap/itdev2/Franc.java
+++ src/main/java/com/koreaap/itdev2/Franc.java
@@ -9,10 +9,4 @@ public class Franc extends Money {
   Franc times(int multiplier) {
     return new Franc(amount * multiplier);
   }
-
-  @Override
-  public boolean equals(Object object) {
-    Money money = (Money) object;
-    return amount == money.amount;
-  }
 }
