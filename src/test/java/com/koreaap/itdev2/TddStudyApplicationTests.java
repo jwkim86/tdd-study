d016db19b093847490917aa178aa563ab7a5b16f
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-08 23:24:38 +0900

    Change test it will be failed

--- src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
+++ src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
@@ -16,6 +16,8 @@ class TddStudyApplicationTests {
     Dollar five = new Dollar(5);
     five.times(2);
     assertEquals(10, five.amount);
+    five.times(3);
+    assertEquals(15, five.amount);
   }
 
   // TODO: $5 + 10CHF = $10. fx rate 2:1
