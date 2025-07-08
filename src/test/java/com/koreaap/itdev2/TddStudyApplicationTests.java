744a40876772e0034db01d4109e80f5adadb09a1
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-08 23:59:24 +0900

    Remove comment TODO: change amount to private

--- src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
+++ src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
@@ -25,7 +25,6 @@ class TddStudyApplicationTests {
   }
 
   // TODO: $5 + 10CHF = $10. fx rate 2:1
-  // TODO: change amount to private
   // TODO: Fix Money round-up error
   // TODO: hashCode()
   // TODO: Equals null
