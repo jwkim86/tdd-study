a6d426b94e855195c4a02c46bd912adaf31b91dd
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-08 23:26:54 +0900

    Fix compile error

--- src/main/java/com/koreaap/itdev2/Dollar.java
+++ src/main/java/com/koreaap/itdev2/Dollar.java
@@ -6,8 +6,9 @@ public class Dollar {
     this.amount = amount;
   }
 
-  void times(int multiplier) {
+  Dollar times(int multiplier) {
     amount *= multiplier;
+    return null;
   }
 
   int amount;
