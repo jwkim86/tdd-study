6db97df2fb7ec95c9851f9c3e308e357dd524d64
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-09 00:29:30 +0900

    Change franc to money in Franc.equals

--- src/main/java/com/koreaap/itdev2/Franc.java
+++ src/main/java/com/koreaap/itdev2/Franc.java
@@ -12,7 +12,7 @@ public class Franc extends Money {
 
   @Override
   public boolean equals(Object object) {
-    Money dollar = (Money) object;
-    return amount == dollar.amount;
+    Money money = (Money) object;
+    return amount == money.amount;
   }
 }
