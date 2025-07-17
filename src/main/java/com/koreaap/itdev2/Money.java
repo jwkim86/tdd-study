95fc31b34b0816491bff31667cf1ab3fffc7eb1d
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-18 01:01:12 +0900

    Change Money class to concrete class

--- src/main/java/com/koreaap/itdev2/Money.java
+++ src/main/java/com/koreaap/itdev2/Money.java
@@ -1,6 +1,6 @@
 package com.koreaap.itdev2;
 
-public abstract class Money {
+public class Money {
 
   protected int amount;
   protected String currency;
@@ -24,7 +24,9 @@ public abstract class Money {
     return new Franc(amount, "CHF");
   }
 
-  abstract Money times(int multiplier);
+  Money times(int multiplier) {
+    return null;
+  }
 
   String currency() {
     return currency;
