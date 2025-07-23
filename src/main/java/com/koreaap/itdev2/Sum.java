344cb522e2ea6157b0c5e2c0d01fe25695321bc1
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-23 23:17:16 +0900

    Implement Sum.constructor

--- src/main/java/com/koreaap/itdev2/Sum.java
+++ src/main/java/com/koreaap/itdev2/Sum.java
@@ -1,7 +1,10 @@
 package com.koreaap.itdev2;
 
 public class Sum implements Expression {
-  public Sum(Money augend, Money addend) {}
+  public Sum(Money augend, Money addend) {
+    this.augend = augend;
+    this.addend = addend;
+  }
 
   Money augend;
   Money addend;
