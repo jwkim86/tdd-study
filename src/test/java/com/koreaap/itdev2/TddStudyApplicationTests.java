08a38e254e41289ec2507769be96e3d0abcef683
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-23 22:25:06 +0900

    Add bank to testSimpleAddition

--- src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
+++ src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
@@ -36,6 +36,7 @@ class TddStudyApplicationTests {
     Money sum = Money.dollar(5).plus(Money.dollar(5));
     assertEquals(Money.dollar(10), sum);
 
+    Money reduced = bank.reduce(sum, "usd");
     assertEquals(Money.dollar(10), reduced);
   }
 
