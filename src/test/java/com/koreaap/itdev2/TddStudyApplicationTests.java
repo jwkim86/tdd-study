6f9bbf8369bde3539da07193e13f426964479732
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-23 22:32:07 +0900

    Add new five dollar to testSimpleAddition. it also made compile error

--- src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
+++ src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
@@ -36,6 +36,7 @@ class TddStudyApplicationTests {
     Money sum = Money.dollar(5).plus(Money.dollar(5));
     assertEquals(Money.dollar(10), sum);
 
+    Money five = Money.dollar(5);
     Expression sum = five.plus(five);
     Bank bank = new Bank();
     // Why not like that Money reduce = sum.reduce(bank, "usd")?
