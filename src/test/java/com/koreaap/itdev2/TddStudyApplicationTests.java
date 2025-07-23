2253a6ca9adea596d1cb87a0ac9f03c8608beb95
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-23 23:19:55 +0900

    Add testReduceSum

--- src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
+++ src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
@@ -50,6 +50,14 @@ class TddStudyApplicationTests {
     assertEquals(five, sum.addend);
   }
 
+  @Test
+  public void testReduceSum() {
+    Expression sum = new Sum(Money.dollar(3), Money.dollar(4));
+    Bank bank = new Bank();
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(7), result);
+  }
+
   // TODO: $5 + 10CHF = $10. fx rate 2:1
   // TODO: $5 + $5 = $10
   // TODO: Fix Money round-up error
