26a019538876f07dfbde02e7900ac4b403182211
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-23 23:21:41 +0900

    Implement Bank.reduce() and remove stub

--- src/main/java/com/koreaap/itdev2/Bank.java
+++ src/main/java/com/koreaap/itdev2/Bank.java
@@ -3,6 +3,8 @@ package com.koreaap.itdev2;
 public class Bank {
 
   public Money reduce(Expression source, String to) {
-    return Money.dollar(10);
+    Sum sum = (Sum) source;
+    int amount = sum.augend.amount + sum.addend.amount;
+    return new Money(amount, to);
   }
 }
