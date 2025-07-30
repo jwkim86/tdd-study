d3f469dfdbad52d4080ca71a969647e839d9fe94
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-30 22:40:38 +0900

    Add Pair class

---
+++ src/main/java/com/koreaap/itdev2/Pair.java
@@ -0,0 +1,11 @@
+package com.koreaap.itdev2;
+
+public class Pair {
+    private String from;
+    private String to;
+
+    Pair(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+}
