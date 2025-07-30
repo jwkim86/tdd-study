a25f175ed49c6381f2ffd3f9c25316840c9977ea
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-30 22:22:46 +0900

    Add test testReduceMoneyDifferentCurrency

--- src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
+++ src/test/java/com/koreaap/itdev2/TddStudyApplicationTests.java
@@ -65,6 +65,14 @@ class TddStudyApplicationTests {
     assertEquals(Money.dollar(1), result);
   }
 
+  @Test
+  public void testReduceMoneyDifferentCurrency() {
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Money result = bank.reduce(Money.franc(2), "USD");
+    assertEquals(Money.dollar(1), result);
+  }
+
   // TODO: $5 + 10CHF = $10. fx rate 2:1
   // TODO: $5 + $5 = $10
   // TODO: $5 + $5에서 Money 반환하기
