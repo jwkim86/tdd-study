b8d5ce7f3c3e9dc725cee28584a148e50c0c9e34
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-30 22:33:01 +0900

    Add Bank parameter to Expression.reduce()

--- src/main/java/com/koreaap/itdev2/Bank.java
+++ src/main/java/com/koreaap/itdev2/Bank.java
@@ -3,7 +3,7 @@ package com.koreaap.itdev2;
 public class Bank {
 
   public Money reduce(Expression source, String to) {
-    return source.reduce(to);
+    return source.reduce(this, to);
   }
 
   public void addRate(String chf, String usd, int i) {}
