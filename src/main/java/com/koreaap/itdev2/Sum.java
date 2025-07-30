34c4990ecb143b23d3a39d754502d58bb27dcc9f
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-07-30 23:05:34 +0900

    Change from Money to Expression class in Sum class

--- src/main/java/com/koreaap/itdev2/Sum.java
+++ src/main/java/com/koreaap/itdev2/Sum.java
@@ -1,13 +1,13 @@
 package com.koreaap.itdev2;
 
 public class Sum implements Expression {
-  public Sum(Money augend, Money addend) {
+  public Sum(Expression augend, Expression addend) {
     this.augend = augend;
     this.addend = addend;
   }
 
-  Money augend;
-  Money addend;
+  Expression augend;
+  Expression addend;
 
   public Money reduce(Bank bank, String to) {
     int amount = augend.reduce(bank, to).amount + addend.reduce(bank, to).amount;
