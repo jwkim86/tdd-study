98ec2f219f3a05914370dac8702ea335d23a5bfb
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-09-03 21:18:58 +0900

    Refactoring one more...

--- src/main/java/com/koreaap/itdev2/appendix/Fibonacci.java
+++ src/main/java/com/koreaap/itdev2/appendix/Fibonacci.java
@@ -3,7 +3,7 @@ package com.koreaap.itdev2.appendix;
 public class Fibonacci {
   public static int fib(int i) {
     if (i == 0) return 0;
-    if (i <= 2) return 1;
+    if (i == 1) return 1;
     return fib(i - 1) + fib(i - 2);
   }
 }
