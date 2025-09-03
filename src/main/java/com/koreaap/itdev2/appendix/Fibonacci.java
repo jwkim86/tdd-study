099df7d62fd5d5caa55d27c5050f5c64658bb9c0
joongwon.kim <roi.joongwon.kim@gmail.com>
2025-09-03 21:18:15 +0900

    Refactoring second stub value

--- src/main/java/com/koreaap/itdev2/appendix/Fibonacci.java
+++ src/main/java/com/koreaap/itdev2/appendix/Fibonacci.java
@@ -4,6 +4,6 @@ public class Fibonacci {
   public static int fib(int i) {
     if (i == 0) return 0;
     if (i <= 2) return 1;
-    return fib(i - 1) + 1;
+    return fib(i - 1) + fib(i - 2);
   }
 }
